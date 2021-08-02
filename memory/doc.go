5a182ff4a6c7ea129b50c5d780ab1ea3fb4b923c
Scott Cotton <scott@mindowl.com>
2021-08-02 23:00:58 +0200

    memory pkg doc

--- memory/doc.go
+++ memory/doc.go
@@ -12,6 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package memory defines the memory model of pal
-// and associated operations.
+// Package memory defines the memory model of pal and associated operations.
+//
+// The memory model is basically a directed graph, where the nodes represent
+// disjoint memory regions (such as the locations of local variables, or the
+// code location of an allocation), and the edges represents a "may points to"
+// relation.  In other words for nodes a,b where (a,b) is an edge, the memory
+// model indicates that in some execution of the analysed program, a may point
+// to b.
+//
+// Separate memory models are intended to be associated with every package
+// under analysis (and we need depended upon packages transitively as well).
+//
+// The above description is a simplification of what is implemented here.
+// First, nodes represent structured data.  Second, the edges are coded as a
+// set of constraints rather than explicitly.  Third, each model represents a
+// single (possibly main) package.  Fourth, the node sizes are parameterised on
+// a values type which may be used to model sizes and indices.  Finally, some
+// nodes are marked "Opaque" indicating that they represent unknown pointers.
+// Correspondingly, there is a (work in progress) mechanism to compose models
+// accross packages which allows eliminating local variables and substituting
+// opaque nodes with results in a calling package.
 package memory
