d4cf2bcdccb7ddc5ef5efb094231d15d6cb500eb
Scott Cotton <scott@mindowl.com>
2021-08-03 04:33:40 +0200

    'plain' codec for srcinfo

--- results/srcinfo.go
+++ results/srcinfo.go
@@ -15,10 +15,12 @@
 package results
 
 import (
+	"fmt"
 	"go/token"
 	"io"
 
 	"github.com/go-air/pal/internal/byteorder"
+	"github.com/go-air/pal/internal/palio"
 )
 
 type SrcKind int
@@ -28,6 +30,7 @@ const (
 	SrcFunc
 	SrcMakeArray
 	SrcMakeSlice
+	SrcMakeMap
 	SrcMakeChan
 
 	SrcMakeInterface
@@ -37,12 +40,80 @@ const (
 	SrcAddressOf
 )
 
+var k2s = map[SrcKind]string{
+	SrcVar:           "var",
+	SrcFunc:          "fun",
+	SrcMakeArray:     "arr",
+	SrcMakeSlice:     "sli",
+	SrcMakeChan:      "chn",
+	SrcMakeMap:       "map",
+	SrcMakeInterface: "int",
+	SrcMakeClosure:   "clo",
+	SrcNew:           "new",
+	SrcAddressOf:     "adr"}
+
+var s2k = map[string]SrcKind{
+	"var": SrcVar,
+	"fun": SrcFunc,
+	"arr": SrcMakeArray,
+	"sli": SrcMakeSlice,
+	"chn": SrcMakeChan,
+	"map": SrcMakeMap,
+	"int": SrcMakeInterface,
+	"clo": SrcMakeClosure,
+	"new": SrcNew,
+	"adr": SrcAddressOf}
+
+func (k SrcKind) String() string {
+	return k2s[k]
+}
+
+func (k SrcKind) PlainEncode(w io.Writer) error {
+	buf := []byte(k.String())
+	_, e := w.Write(buf)
+	return e
+}
+
+func (k *SrcKind) PlainDecode(r io.Reader) error {
+	buf := make([]byte, 3)
+	_, e := palio.ReadBuf(buf, r)
+	if e != nil {
+		return e
+	}
+	var present bool
+	*k, present = s2k[string(buf)]
+	if !present {
+		return fmt.Errorf("unknown srckind: %s", string(buf))
+	}
+	return nil
+}
+
 // SrcInfo represents the information about
 // the source code pal expects to extract
 // from an ir such as golang.org/x/tools/go/ssa.
 type SrcInfo struct {
 	Kind SrcKind
 	Pos  token.Pos
+}
+
+func (si *SrcInfo) PlainEncode(w io.Writer) error {
+	var err error
+	if err = si.Kind.PlainEncode(w); err != nil {
+		return err
+	}
+	_, err = fmt.Fprintf(w, " %08x", si.Pos)
+	return err
+}
+
+func (si *SrcInfo) PlainDecode(r io.Reader) error {
+	var err error
+	p := &si.Kind
+	if err = p.PlainDecode(r); err != nil {
+		return err
+	}
+	q := &si.Pos
+	_, err = fmt.Fscanf(r, " %08x", q)
+	return err
 }
 
 // PalEncode encodes si onto w, returning
